42a562b235c47db49fad279542bc95f0f73059a9
SedaNurPOLATER <snpolater07@gmail.com>
2024-01-26 19:19:02 +0300

    carbook

---
+++ Core/UdemyCarBook.Core.Application/Features/CQRS/Commands/BrandCommands/CreateBrandCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Core.Application.Features.CQRS.Commands.BrandCommands
+{
+    public class CreateBrandCommand
+    {
+        public string BrandName { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Core/UdemyCarBook.Core.Application/Features/CQRS/Results/BrandResults/GetBrandQueryResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Core.Application.Features.CQRS.Results.BrandResults
+{
+    public class GetBrandQueryResult
+    {
+        public int BrandID { get; set; }
+        public string BrandName { get; set; }
+        public bool Status { get; set; }
+    }
+}
